3fdd9f73365048bc3d162c494f2adcfc41109a49
Clem <clem.ssbm@hotmail.fr>
2024-12-18 21:20:03 +0100

    Updates

--- src/AppStyles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledApp = styled.div`
-  display: grid;
-  grid-gap: 20px;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  padding: 20px;
-`;
