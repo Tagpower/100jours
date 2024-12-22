a876ea6cc40f456eeb9b1099170ba1395cbf09f8
Clem <clem.ssbm@hotmail.fr>
2024-12-22 18:04:06 +0100

    dev branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { createGlobalStyle } from "styled-components";
-import { StyledApp } from "./AppStyles";
-import { createCalendar } from "./helpers";
-import Case from "./case";
-import Appbackground from "./img/denim.png"
+import allItems from "./data";
+import Appbackground from "./img/denim.png";
+import { Chrono } from "react-chrono";
+import Jour from "./Jour";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -22,54 +22,22 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
 
-  const [doors, setDoors] = useState([]);
-  const [openDoors, setOpenDoors] = useState([]);
-
-  useEffect(() => {
-    
-    const calendar = localStorage.calendar
-      ? JSON.parse(localStorage.calendar)
-      : createCalendar();
-
-    setDoors(calendar);
-  }, []);
-
-  // Store calendar in localStorage
-  useEffect(() => {
-    doors.length && localStorage.setItem("calendar", JSON.stringify(doors));
-  }, [doors]);
-
-    // Store calendar in localStorage
-    useEffect(() => {
-      openDoors.length && localStorage.setItem("openDoors", JSON.stringify(openDoors));
-    }, [openDoors]);
-
-  const handleFlipDoor = id => {
-    const updatedDoors = doors.map(door =>
-        door.id === id ? { ...door, open: !door.open } : door
-    );
-    setDoors(updatedDoors);
-    setOpenDoors(updatedDoors.filter(d => d.open).map(d => d.nr))
-
-  };
-
-
   return (
     <>
       <GlobalStyle />
       <header>
-        <h1>Vivement l'acquis !</h1>
-        </header>
-      <StyledApp>
-        {doors.map(door => (
-          <Case
-            key={door.id}
-            caseData={door}
-            open={openDoors.findIndex(d => d.id === door.id)}
-            handleClick={handleFlipDoor}
-          />
-        ))}
-      </StyledApp>
+        <h1>Calendrier de l'avant-retraite</h1>
+        <h2>Vivement l'acquis !</h2>
+      </header>
+      <div className="calendrier">
+        <Chrono mode="HORIZONTAL" items={allItems} enableDarkToggle cardWidth={600} cardHeight={480}>
+           {/* <div className="chrono-icons">
+              {allItems.map((jour) => <strong>{jour.num}</strong>)}
+            </div> */}
+            {/* {allItems.map(jour => <Jour item={jour} key={jour.num}/>)} */}
+        </Chrono>
+      </div>
+
     </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,37 @@
+var allItems = [
+  {
+    title: "100",
+    num: 100,
+    cardTitle: "22 dec 2024",
+    cardSubtitle : "bla",
+    cardDetailedText: "bloqwgbiobgiowbgoasg",
+    media: {
+      type: "IMAGE",
+      source: {url: "https://drive.google.com/file/d/1HiNHL2pOWSfRXW7qlkQod8ya4wKqIyXm/view"}
+    }
+  },
+  {
+    title: "99",
+    num: 99,
+    cardTitle: "23 dec 2024",
+    cardSubtitle : "bla2",
+    cardDetailedText: "bloqwgbiobgiowbgoasg3",
+    media: {
+      type: "IMAGE",
+      source: "img/gne.jpg"
+    }
+  },
+  {
+    title: "98",
+    num: 98,
+    cardTitle: "24 dec 2024",
+    cardSubtitle : "Noel",
+    cardDetailedText: "",
+    media: {
+      type: "IMAGE",
+      source: {url: "img/gne.jpg"}
+    }
+  }
+];
+
+export default allItems; 
