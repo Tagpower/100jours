1afd68ab8a8aeced0b49b2355d3c9f04cb28daea
Clément Bauchet <clement.bauchet@emmicroelectronic.com>
2024-12-23 17:17:43 +0100

    update 23 dec

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ import buildAllItems from "./data";
 const GlobalStyle = createGlobalStyle`
   body {
     background: center fixed url(${Appbackground});
-    margin: 0;
+    margin: 1em;
   }
   header {
     display: flex,
     align-items: center;
     justify-content: center;
     text-align: center;
-    color: white
-
+    color: white;
+    flex-direction: column;
+    font-size: calc(10px + 2vmin);
   }
 `;
 
@@ -24,16 +25,21 @@ function App() {
 
   const [allItems, setItems] = useState(buildAllItems());
 
+  const daysLeft = allItems.find((item) => item.locked).num + 1;
+  console.log("daysLeft", daysLeft);
+
   return (
     <>
       <GlobalStyle />
       <header>
         <h1>Calendrier de l'avant-retraite</h1>
         <h4>Vivement l'acquis !</h4>
-        {console.log(allItems)}
       </header>
       <div className="calendrier">
-        <Chrono mode="HORIZONTAL" items={allItems} enableDarkToggle titleDateFormat="DD.MM.YYYY" cardWidth={500} cardHeight={500} timelinePointShape="circle" timelinePointDimension={30} highlightCardsOnHover >
+        <Chrono mode="HORIZONTAL" items={allItems} allowDynamicUpdate focusActiveItemOnLoad 
+        enableBreakPoint enableDarkToggle timelinePointShape="circle" timelinePointDimension={30} highlightCardsOnHover activeItemIndex={0} parseDetailsAsHTML
+        theme={{secondary: 'blue', titleColorActive: 'white'}}
+        titleDateFormat="DD.MM.YYYY" cardWidth={625} cardHeight={500} mediaSettings={{ align: 'center', fit: 'cover' }} >
            <div className="chrono-icons">
               {allItems.map((jour) => <strong>{jour.num}</strong>)}
             </div>
--- src/data.js
+++ src/data.js
@@ -1,4 +1,10 @@
-function buildItem(num, media) {
+var contents = [
+  {subtitle:"yo", text:"<p>Voilà</p>", type:"IMAGE", url:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Test.svg/2560px-Test.svg.png"},
+  {text:"<h1>Bah</h1>", type:"IMAGE", url:"https://cdn.discordapp.com/attachments/788050249356017725/1007350979429154886/IMG_20220811_201113.jpg?ex=6769a835&is=676856b5&hm=f6d5550a9142afa6417aecedea8536b8e16cdf9acac5e52d90e77fb733eecfcc&"},
+
+]
+
+function buildItem(num, content) {
   var date= new Date("2025-04-01");
   date.setDate(date.getDate()-num);
 
@@ -7,10 +13,12 @@ function buildItem(num, media) {
     date: date,
     locked: (new Date() < date),
     cardTitle: date.toLocaleDateString() + " - Encore " + num + " jours !" ,
+    cardSubtitle: content.subtitle || "",
+    cardDetailedText: content.text,
     media: {
-      type: "IMAGE",
+      type: content.type,
       source: {
-        url: media
+        url: content.url
       }
     }
   }
@@ -19,8 +27,8 @@ function buildItem(num, media) {
 export default function buildAllItems() {
   var allItems = [];
   for (var i = 100; i >= 0; i--) {
-    allItems.push(buildItem(i, "https://cdn.discordapp.com/attachments/788050249356017725/1007350979429154886/IMG_20220811_201113.jpg?ex=6769a835&is=676856b5&hm=f6d5550a9142afa6417aecedea8536b8e16cdf9acac5e52d90e77fb733eecfcc&"))
+    allItems.push(buildItem(i, contents[(100-i) % contents.length]));
   }
-  console.log("allitems", allItems)
+  //console.log("allitems", allItems)
   return allItems;
 }
